17a447fb560c74a9681018ac7867aa18895addbf
Ilya <step7@comcast.net>
2026-04-02 10:33:20 -0700

    adding ci security

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
+import pluginSecurity from "eslint-plugin-security";
 
 export default defineConfig([
   {
@@ -15,4 +16,5 @@ export default defineConfig([
     },
   },
   tseslint.configs.recommended,
+  pluginSecurity.configs.recommended,
 ]);
